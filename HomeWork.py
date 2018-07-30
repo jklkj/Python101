65bb0b7aff1b34f951eb2697eded0b214c6d91d6
chiahao <jklkj_ya@ayhoo.com.tw>
2018-07-30 21:03:29 +0800

    首次上傳

--- HomeWork.py
+++
@@ -1,8 +0,0 @@
-x1 = int(input('輸入第1個數字: '))
-x2 = int(input('輸入第2個數字: '))
-x3 = int(input('輸入第3個數字: '))
-x4 = int(input('輸入第4個數字: '))
-x5 = int(input('輸入第5個數字: '))
-HomeWork1 = [x1, x2, x3, x4, x5]
-print("最大值 = ",max(HomeWork1))
-print("總和 = " + str(sum(HomeWork1)))
---
+++ HomeWork2.py
@@ -0,0 +1,7 @@
+n1 = str(input("請輸入第一個數字："))
+n2 = str(input("請輸入第二個數字："))
+op = input("請輸入運算元: +, -, *, /")
+print(eval(n1+op+n2))
+
+n1 = input("請輸入一個數字計算其平方根：")
+print(int(n1)**1/2)
